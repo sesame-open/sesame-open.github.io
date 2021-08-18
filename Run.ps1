7ff0e1e17866c9071a9bd0f866f02ea726f2a836
Jordi Borghers <jordi.borghers@tucrail.be>
2021-08-18 16:24:42 +0200

    Updated Run.ps1 script

--- Run.ps1
+++ Run.ps1
@@ -1,1 +1,5 @@
-docker run --rm --volume="$($PWD):/srv/jekyll" -p 4000:4000 jekyll/jekyll:stable jekyll serve --incremental --livereload --force_polling
+docker run --rm --volume="$($PWD):/srv/jekyll" -p 4000:4000 --name jekyll-serve -d jekyll/jekyll:stable jekyll serve --incremental --livereload --force_polling
+docker logs -f jekyll-serve
+
+Write-Host "`nStopping container:"
+docker stop jekyll-serve
